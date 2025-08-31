02403d9de956e5b56213c8943652a6acbfe1e3e1
yosefurr <yosefanwar690@gmail.com>
2025-08-31 15:06:16 +0300

    added navbar & hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
-
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return <div className="flex-center text-7xl h-[100vh]">App</div>;
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  );
 };
 
 export default App;
